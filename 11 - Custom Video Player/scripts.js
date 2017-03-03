e5c77046440510aeb9c5d323aa846edd66124a53
Zach Baker <zach.r.baker1@gmail.com>
2017-03-02 21:54:47 -0700

    👅

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,28 +6,35 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-function togglePlay () {
+function togglePlay() {
 	video[video.paused ? 'play' : 'pause']();
 }
 
-function updateButton () {
+function updateButton() {
 	const icon = this.paused ? '►' : '❚ ❚';
 	toggle.textContent = icon;
 }
 
-function skip () {
-	console.log('skipping biotch')
+function skip() {
 	video.currentTime += parseFloat(this.dataset.skip);
 }
 
 function handleRangeUpdate() {
-	console.log(this.value)
+	video[this.name] = this.value;
 }
+
+function handleProgress() {
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`
+}
+
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(b => b.addEventListener('click', skip));
-ranges.forEach(r => r.addEventListener('click', handleRangeUpdate))
+ranges.forEach(r => r.addEventListener('change', handleRangeUpdate));
+ranges.forEach(r => r.addEventListener('mousemove', handleRangeUpdate));
 
